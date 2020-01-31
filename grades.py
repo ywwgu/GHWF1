73318985b6ce3b6abb99313af21590c3da806409
Yuwen Wang <yww9@hotmail.com>
2020-01-31 14:24:02 -0500

    Compute average for two grades

--- grades.py
+++ grades.py
@@ -2,4 +2,4 @@
 def compute_hw_average(grades):
     if len(grades) == 0:
         return 0
-    return grades[0]
+    return sum(grades) / len(grades)
--- test_grades.py
+++ test_grades.py
@@ -10,3 +10,8 @@ def test_zero_grades():
 def test_single_grade():
     grades = [42]
     assert compute_hw_average(grades) == 42
+
+
+def test_two_grades():
+    grades = [10, 30]
+    assert compute_hw_average(grades) == 20
